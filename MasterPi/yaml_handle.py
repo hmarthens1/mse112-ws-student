548d77515c318f6c90303f1ae55c6a7c64658c0d
hmarthens1 <hmarthens@gmail.com>
2026-06-02 15:20:13 -0700

    yaml path file changed

--- MasterPi/yaml_handle.py
+++ MasterPi/yaml_handle.py
@@ -1,6 +1,9 @@
 import yaml
-lab_file_path = '/home/pi/mse112-ws-student/MasterPi/lab_config.yaml'
-Deviation_file_path = '/home/pi/mse112-ws-student/MasterPi/Deviation.yaml'
+import os
+import sys
+_base = os.path.join(os.path.expanduser('~'), 'mse112-ws-student', 'MasterPi')
+lab_file_path = os.path.join(_base, 'lab_config.yaml')
+Deviation_file_path = os.path.join(_base, 'Deviation.yaml')
 
 def get_yaml_data(yaml_file):
     file = open(yaml_file, 'r', encoding='utf-8')
